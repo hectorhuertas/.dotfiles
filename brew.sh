c83fd35ffb7caa2757ffa5bf3b0b11f2e5fa6507
Hector Huertas <hectorhuertas@gmail.com>
2017-04-07 23:19:48 +0100

    moar programs

--- brew.sh
+++ brew.sh
@@ -6,15 +6,15 @@ brew upgrade
 
 brew tap caskroom/cask
 
-brew cask install google-chrome
 brew cask install atom
+brew cask install dropbox
+brew cask install evernote
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install skype
+brew cask install slack
 brew cask install spectacle
 brew cask install steam
-brew cask install evernote
-brew cask install slack
-brew cask install iterm2
-brew cask install dropbox
-brew cask install skype
 
 brew cleanup
 brew cask cleanup
