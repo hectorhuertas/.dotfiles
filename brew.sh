d32f6d25d9fe8e95893fb772697ad9887d46c18c
Hector Huertas <hectorhuertas@gmail.com>
2017-04-27 21:16:57 +0100

    add programs

--- brew.sh
+++ brew.sh
@@ -4,17 +4,28 @@ brew update
 
 brew upgrade
 
+brew install hub
+brew install node
+brew install tmux
+brew install trash
+brew install unrar
+brew install vim
+brew install zsh
+
 brew tap caskroom/cask
 
 brew cask install atom
+brew cask install docker
 brew cask install dropbox
 brew cask install evernote
 brew cask install google-chrome
 brew cask install iterm2
+brew cask install keepingyouawake
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
 brew cask install steam
+brew cask install vlc
 
 brew cleanup
 brew cask cleanup
