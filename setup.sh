6401d21cb2c530ce5508d87444ffc953868d5702
Hector Huertas <hectorhuertas@gmail.com>
2017-12-06 11:52:43 +0000

    Remove empty lines from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 set -e
 
-echo ""
 echo "--------------------"
 echo "-- Dotfiles setup --"
 echo "--------------------"
 
 if [ ! -x "$HOME/.dotfiles/setup.sh" ]; then
-  echo ""
   echo "ERROR: The dotfiles repository should be cloned at '~/.dotfiles',"
-  echo "         so this script should be at '~/.dotfiles/setup.sh'"
+  echo "       so this script should be at '~/.dotfiles/setup.sh'"
   exit
 fi
 
 if [ "$(pwd)" != "$HOME/.dotfiles" ]; then
-  echo ""
   echo "ERROR: This script must be run from $HOME/.dotfiles"
   exit
 fi
 
-echo ""
 echo "Symlinking config files..."
 
 rm ~/.gitconfig
