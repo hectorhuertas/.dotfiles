4bd0b80bd5dba9ef8e5bebb25786436957dc398b
Hector Huertas <hectorhuertas@gmail.com>
2018-03-19 00:01:29 +0000

    Update brew packages to install

--- brew.sh
+++ brew.sh
@@ -10,14 +10,19 @@ brew update
 brew upgrade
 
 brew install exercism
+#brew install fasd
+brew install fd
+brew install fzf
 brew install hub
 brew install jq
-brew install nmap
-brew install node
+brew install neovim
+#brew install nmap
+#brew install node
+brew install ripgrep
+brew install shellcheck
 brew install tmux
 brew install trash
 brew install unrar
-brew install vim
 brew install wget
 brew install zplug
 brew install zsh
@@ -25,25 +30,25 @@ brew install zsh
 brew tap caskroom/cask
 
 brew cask install atom
-brew cask install battle-net
+#brew cask install battle-net
 brew cask install docker
 brew cask install dropbox
 brew cask install evernote
 brew cask install flux
-brew cask install gog-galaxy
+#brew cask install gog-galaxy
 brew cask install google-chrome
 brew cask install hammerspoon
-brew cask install intel-power-gadget
+#brew cask install intel-power-gadget
 brew cask install iterm2
 brew cask install karabiner-elements
 brew cask install keepingyouawake
 brew cask install keybase
 brew cask install macs-fan-control
-brew cask install qbittorrent
+#brew cask install qbittorrent
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
-brew cask install steam
+#brew cask install steam
 brew cask install tomighty
 brew cask install vlc
 brew cask install yujitach-menumeters
