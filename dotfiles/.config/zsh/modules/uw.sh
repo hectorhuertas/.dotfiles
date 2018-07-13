6e3389867830ef61ee94d5f033747e84ebe086da
Hector Huertas <hectorhuertas@gmail.com>
2018-07-13 18:04:13 +0200

    Add gcp-dev to UW setups

--- dotfiles/.config/zsh/modules/uw.sh
+++ dotfiles/.config/zsh/modules/uw.sh
@@ -2,5 +2,6 @@ function start_ssh_tunnels() {
   autossh -f -M 0 -Nn -D 127.0.0.1:2424 jumpbox.dev.uw.systems
   autossh -f -M 0 -Nn -D 127.0.0.1:2425 jumpbox.prod.uw.systems
   autossh -f -M 0 -Nn -D 127.0.0.1:2426 jumpbox.dev.gcp.uw.systems
+  autossh -f -M 0 -Nn -D 127.0.0.1:2427 jumpbox.prod.gcp.uw.systems
 }
 alias tunnels=start_ssh_tunnels
