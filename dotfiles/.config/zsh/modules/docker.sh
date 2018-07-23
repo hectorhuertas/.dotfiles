e7b2d0032d22d2c80eb009e746c77d03ade3cdd2
Hector Huertas <hectorhuertas@gmail.com>
2018-07-23 12:24:03 +0200

    Deal with poor Docker for Mac dotfiles support

--- dotfiles/.config/zsh/modules/docker.sh
+++ dotfiles/.config/zsh/modules/docker.sh
@@ -1,1 +1,9 @@
-export DOCKER_CONFIG="$HOME/.config/docker"
+if command -v docker > /dev/null; then
+  export DOCKER_CONFIG="$HOME/.config/docker"
+
+  function dotfiles_docker() {
+    rm -rf ~/.docker
+  }
+
+  alias docker='rm -rf ~/.docker && docker'
+fi
