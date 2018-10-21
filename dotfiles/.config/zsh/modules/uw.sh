237e31c17f8057201cfcb25c3ad89fcf3381a6ef
Hector Huertas <hectorhuertas@gmail.com>
2018-10-21 19:35:25 +0100

    Set strongbox home

--- dotfiles/.config/zsh/modules/uw.sh
+++ dotfiles/.config/zsh/modules/uw.sh
@@ -1,3 +1,5 @@
+export STRONGBOX_HOME="$HOME/.config/strongbox"
+
 function start_ssh_tunnels() {
   autossh -f -M 0 -Nn -D 127.0.0.1:2424 jumpbox.dev.uw.systems
   autossh -f -M 0 -Nn -D 127.0.0.1:2425 jumpbox.prod.uw.systems
