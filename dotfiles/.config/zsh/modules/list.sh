912d39ee95bb5df264eee4ba21debabb0a213b50
Hector Huertas <hectorhuertas@gmail.com>
2018-10-21 20:03:19 +0100

    Remove .DS_STORE responsibility from list functions

--- dotfiles/.config/zsh/modules/list.sh
+++ dotfiles/.config/zsh/modules/list.sh
@@ -1,12 +1,12 @@
 function list {
-  rm -f .DS_Store && ls -AF $1
+  ls -AF $1
 }
 
 alias l=list
 
 function list_long {
   # Only valid for BSD 'ls'. GNU 'ls' has different meaning for '-G' flag
-  rm -f .DS_Store && ls -AGlh $1
+  ls -AGlh $1
 }
 
 alias ll=list_long
