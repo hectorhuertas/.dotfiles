7cb39e89dd7a0d72f02390ebecdd57b6caee7005
Hector Huertas <hectorhuertas@gmail.com>
2019-03-03 15:51:25 +0100

    Remove bash and less files with cron

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,6 +1,10 @@
 # Disable cron mails
 crontab -l | grep 'MAILTO=""' || (crontab -l 2>/dev/null; echo 'MAILTO=""') | crontab -
 # Remove unwanted docker folder
-crontab -l | grep '* * * * * rm -rf ~/.docker' || (crontab -l 2>/dev/null; echo "* * * * * rm -rf ~/.docker") | crontab -
+crontab -l | grep "* * * * * rm -rf ~/.docker" || (crontab -l 2>/dev/null; echo "* * * * * rm -rf ~/.docker") | crontab -
 # Remove all .DS_Store files
 crontab -l | grep "* * * * * find ~ -name '.DS_Store' -type f -delete" || (crontab -l 2>/dev/null; echo "* * * * * find ~ -name '.DS_Store' -type f -delete") | crontab -
+# Remove bash files
+crontab -l | grep "* * * * * rm -rf ~/.bash_sessions; rm ~/.bash_history" || (crontab -l 2>/dev/null; echo "* * * * * rm -rf ~/.bash_sessions; rm ~/.bash_history") | crontab -
+# Remove lesshist file
+crontab -l | grep "* * * * * rm ~/.lesshst" || (crontab -l 2>/dev/null; echo "* * * * * rm ~/.lesshst") | crontab -
--- dotfiles/.config/zsh/modules/less.sh
+++
@@ -1,3 +0,0 @@
-# Setup plugin's extra configurations
-mkdir -p ~/.local/share/less
-LESSHISTFILE=$HOME/.local/share/less/history
