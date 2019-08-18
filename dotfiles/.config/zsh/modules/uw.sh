68746b68abc7491e2a3b994e2fe8da9959987c3b
Hector Huertas <hectorhuertas@gmail.com>
2019-08-18 19:16:18 +0200

    Update uw zsh module

--- dotfiles/.config/zsh/modules/uw.sh
+++ dotfiles/.config/zsh/modules/uw.sh
@@ -1,9 +1,12 @@
 export STRONGBOX_HOME="$HOME/.config/strongbox"
 
 function start_ssh_tunnels() {
+  # These won't work if the jumpbox keys are not already accepted
   autossh -f -M 0 -Nn -D 127.0.0.1:2424 jumpbox.dev.uw.systems
   autossh -f -M 0 -Nn -D 127.0.0.1:2425 jumpbox.prod.uw.systems
   autossh -f -M 0 -Nn -D 127.0.0.1:2426 jumpbox.dev.gcp.uw.systems
   autossh -f -M 0 -Nn -D 127.0.0.1:2427 jumpbox.prod.gcp.uw.systems
 }
 alias tunnels=start_ssh_tunnels
+
+alias uw='cd ~/Dropbox/info/uw'
