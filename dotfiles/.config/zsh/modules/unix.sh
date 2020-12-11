7f5bdbb090aa356edaecb615ce842cfa7bf8c650
Hector Huertas <hectorhuertas@gmail.com>
2020-12-11 19:05:53 +0100

    Cleanup zsh

--- dotfiles/.config/zsh/modules/unix.sh
+++ dotfiles/.config/zsh/modules/unix.sh
@@ -22,18 +22,6 @@ function parse_flag_hh(){
     | awk '{print $1}'
 }
 
-# https://vt100.net/docs/vt510-rm/DECSCUSR.html
-function blink_cursor_hh(){
-  times=10
-  while [[ $times != 0 ]];do
-    printf "\e[4 q"
-    sleep 0.05
-    printf "\e[2 q"
-    sleep 0.05
-    ((times = times - 1))
-  done
-}
-
 # https://stackoverflow.com/questions/2829613/how-do-you-tell-if-a-string-contains-another-string-in-posix-sh
 contains() {
     string="$1"
