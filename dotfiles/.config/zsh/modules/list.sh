20dd70423f93519b9b412cfcc6c4348fc1ee3226
Hector Huertas <hectorhuertas@gmail.com>
2021-01-13 11:50:54 +0100

    Make ls faster by removing git status

--- dotfiles/.config/zsh/modules/list.sh
+++ dotfiles/.config/zsh/modules/list.sh
@@ -1,7 +1,8 @@
-alias l="exa --long --git --color-scale --all --ignore-glob=.git"
-alias ll="exa --long --git --color-scale --all --ignore-glob=.git --tree --level=2"
-alias lll="exa --long --git --color-scale --all --ignore-glob=.git --tree --level=3"
-alias llll="exa --long --git --color-scale --all --ignore-glob=.git --tree --level=4"
+alias lg="exa --long --git --color-scale --all --ignore-glob=.git"
+alias l="exa --long --color-scale --all --ignore-glob=.git"
+alias ll="exa --long --color-scale --all --ignore-glob=.git --tree --level=2"
+alias lll="exa --long --color-scale --all --ignore-glob=.git --tree --level=3"
+alias llll="exa --long --color-scale --all --ignore-glob=.git --tree --level=4"
 
 # To be used by other utilities like cd or z
 function list {
